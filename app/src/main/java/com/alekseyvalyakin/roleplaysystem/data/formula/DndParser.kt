6275efc0c60111a0844c585da52b9feee589f7dd
xzaleksey <xzaleksey@gmail.com>
2018-10-13 22:47:52 +0300

    Refactoring.

--- app/src/main/java/com/alekseyvalyakin/roleplaysystem/data/formula/DndParser.kt
+++ app/src/main/java/com/alekseyvalyakin/roleplaysystem/data/formula/DndParser.kt
@@ -5,11 +5,11 @@ class DndParser(
         private val value: Double
 ) : FormulaParser {
 
-    private val modificator = (value - value % 2 - 10) / 2
+    private val modifier = (value - value % 2 - 10) / 2
 
     override fun parse(string: String): FormulaPart? {
         if (string == this.string) {
-            return Number(modificator)
+            return Number(modifier)
         }
 
         return null
