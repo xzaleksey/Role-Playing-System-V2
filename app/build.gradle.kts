754b377abc1f7dead31961676696010b1db38d63
xzaleksey <xzaleksey@gmail.com>
2019-09-15 00:32:59 +0300

    Added debuggable release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 import Versions.anko_version
 import Versions.autodispose_version
 import Versions.room_version
+import java.io.FileInputStream
+import java.util.*
 
 plugins {
     id("com.android.application")
@@ -17,9 +19,9 @@ apply {
 
 apply(from = "master_password.gradle")
 
-//val keystorePropertiesFile = rootProject.file("keystore.properties")
-//val keystoreProperties = java.util.Properties()
-//keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 
 android {
@@ -29,8 +31,8 @@ android {
         applicationId = "com.alekseyvalyakin.roleplaysystem"
         minSdkVersion(21)
         targetSdkVersion(28)
-        versionCode = 12
-        versionName = "1.1.2"
+        versionCode = 15
+        versionName = "1.1.3"
         buildConfigField("String", "MASTER_PASSWORD", "\"$project.password\"")
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
 
@@ -41,28 +43,30 @@ android {
         }
     }
 
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+
+            storePassword = keystoreProperties.getProperty("storePassword")
+        }
+    }
+
     buildTypes {
         get("release").apply {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFile(getDefaultProguardFile("proguard-android.txt"))
             proguardFile(file("proguard-rules.pro"))
-//            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("release")
+            isDebuggable = true
         }
         get("debug").apply {
             ext["alwaysUpdateBuildId"] = false
         }
     }
-
-    signingConfigs {
-        //        getByName("release") {
-//            keyAlias = keystoreProperties["keyAlias"]
-//            keyPassword = keystoreProperties["keyPassword"]
-//            storeFile = file(keystoreProperties["storeFile"])
-//            storePassword = keystoreProperties["storePassword"]
-//        }
-    }
-
 
     compileOptions {
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -100,58 +104,58 @@ dependencies {
     implementation("com.jakewharton.rxbinding2:rxbinding-recyclerview-v7:2.1.1")
     implementation("com.jakewharton.timber:timber:4.7.1")
     implementation("net.danlew:android.joda:2.10.1.1")
-    implementation ("com.google.android.play:core:1.6.3")
+    implementation("com.google.android.play:core:1.6.3")
     implementation("android.arch.work:work-runtime:1.0.1")
 //// if you want some benefits from Kotlin then try this
-    implementation ("android.arch.work:work-runtime-ktx:1.0.1")
+    implementation("android.arch.work:work-runtime-ktx:1.0.1")
 
 //    //Room
-    implementation ("android.arch.persistence.room:runtime:$room_version")
-    kapt ("android.arch.persistence.room:compiler:$room_version")
-    testImplementation ("android.arch.persistence.room:testing:$room_version")
-    implementation ("android.arch.persistence.room:rxjava2:$room_version")
+    implementation("android.arch.persistence.room:runtime:$room_version")
+    kapt("android.arch.persistence.room:compiler:$room_version")
+    testImplementation("android.arch.persistence.room:testing:$room_version")
+    implementation("android.arch.persistence.room:rxjava2:$room_version")
 //
 //    // sdk15, sdk19, sdk21, sdk23 are also available
-    implementation ("org.jetbrains.anko:anko-commons:$anko_version")
-    implementation ("org.jetbrains.anko:anko-sdk25:$anko_version")
-    implementation ("org.jetbrains.anko:anko-appcompat-v7:$anko_version")
-    implementation ("org.jetbrains.anko:anko-cardview-v7:$anko_version")
-    implementation ("org.jetbrains.anko:anko-design:$anko_version")
-    implementation ("org.jetbrains.anko:anko-recyclerview-v7:$anko_version")
-    implementation ("org.jetbrains.anko:anko-constraint-layout:$anko_version")
+    implementation("org.jetbrains.anko:anko-commons:$anko_version")
+    implementation("org.jetbrains.anko:anko-sdk25:$anko_version")
+    implementation("org.jetbrains.anko:anko-appcompat-v7:$anko_version")
+    implementation("org.jetbrains.anko:anko-cardview-v7:$anko_version")
+    implementation("org.jetbrains.anko:anko-design:$anko_version")
+    implementation("org.jetbrains.anko:anko-recyclerview-v7:$anko_version")
+    implementation("org.jetbrains.anko:anko-constraint-layout:$anko_version")
 
-    implementation ("org.jetbrains.anko:anko-appcompat-v7-coroutines:$anko_version")
-    implementation ("org.jetbrains.anko:anko-sdk25-coroutines:$anko_version")
-    implementation ("org.jetbrains.anko:anko-design-coroutines:$anko_version")
-    implementation ("com.google.android:flexbox:1.0.0")
+    implementation("org.jetbrains.anko:anko-appcompat-v7-coroutines:$anko_version")
+    implementation("org.jetbrains.anko:anko-sdk25-coroutines:$anko_version")
+    implementation("org.jetbrains.anko:anko-design-coroutines:$anko_version")
+    implementation("com.google.android:flexbox:1.0.0")
 
-    implementation ("com.nineoldandroids:library:2.4.0")
-    implementation ("com.github.bumptech.glide:glide:4.7.1")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.7.1")
-    implementation ("id.zelory:compressor:2.1.0")
-    implementation ("com.github.chrisbanes:PhotoView:2.1.4")
+    implementation("com.nineoldandroids:library:2.4.0")
+    implementation("com.github.bumptech.glide:glide:4.7.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.7.1")
+    implementation("id.zelory:compressor:2.1.0")
+    implementation("com.github.chrisbanes:PhotoView:2.1.4")
 
 
     implementation("com.kbeanie:multipicker:1.5@aar")
-    implementation ("com.afollestad.material-dialogs:core:2.0.0-alpha07")
-    implementation ("com.afollestad.material-dialogs:input:2.0.0-alpha07")
-    implementation ("com.github.tbruyelle:rxpermissions:0.10.2")
-    implementation( "com.jakewharton.rxrelay2:rxrelay:2.1.0")
-    implementation( "eu.davidea:flexible-adapter:5.0.0-rc1")
-    implementation ("com.daimajia.easing:library:2.0@aar")
-    implementation ("com.daimajia.androidanimations:library:2.3@aar")
+    implementation("com.afollestad.material-dialogs:core:2.0.0-alpha07")
+    implementation("com.afollestad.material-dialogs:input:2.0.0-alpha07")
+    implementation("com.github.tbruyelle:rxpermissions:0.10.2")
+    implementation("com.jakewharton.rxrelay2:rxrelay:2.1.0")
+    implementation("eu.davidea:flexible-adapter:5.0.0-rc1")
+    implementation("com.daimajia.easing:library:2.0@aar")
+    implementation("com.daimajia.androidanimations:library:2.3@aar")
 
-    implementation ("com.google.android.exoplayer:exoplayer:2.9.0")
-    implementation ("com.google.android.exoplayer:exoplayer-ui:2.9.0")
+    implementation("com.google.android.exoplayer:exoplayer:2.9.0")
+    implementation("com.google.android.exoplayer:exoplayer-ui:2.9.0")
 
-    kapt ("com.uber.rib:rib-compiler-test:0.9.1")
+    kapt("com.uber.rib:rib-compiler-test:0.9.1")
 
-    implementation ("com.uber.autodispose:autodispose-android:$autodispose_version@aar")
-    testImplementation ("com.uber.rib:rib-test-utils:0.9.1")
+    implementation("com.uber.autodispose:autodispose-android:$autodispose_version@aar")
+    testImplementation("com.uber.rib:rib-test-utils:0.9.1")
 
-    testImplementation ("junit:junit:4.12")
-    androidTestImplementation ("com.android.support.test:runner:1.0.2")
-    androidTestImplementation ("com.android.support.test.espresso:espresso-core:3.0.2")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
 }
 
 configurations {
--- app/src/main/java/com/rxfirebase2/RxAppUpdate.kt
+++ app/src/main/java/com/rxfirebase2/RxAppUpdate.kt
@@ -4,13 +4,17 @@ import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import io.reactivex.Completable
 import io.reactivex.Single
+import timber.log.Timber
 
 
 fun AppUpdateManager.getAppUpdateInfoSingle(): Single<AppUpdateInfo> {
     return Single.create { emitter ->
+        Timber.d("appUpdate: request updateInfo")
         appUpdateInfo.addOnSuccessListener {
+            Timber.d("appUpdate: got update info $it")
             emitter.onSuccess(it)
         }.addOnFailureListener {
+            Timber.d("appUpdate: got update error $it")
             emitter.onError(it)
         }
     }
